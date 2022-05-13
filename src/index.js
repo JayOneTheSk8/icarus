eebcccd89543a0850c3e66092ec935ffad44a965
JayOneTheSk8 <misterjustincox@gmail.com>
2022-05-13 13:46:36 -0400

    create javascript and bundle

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import chapters from "./chapters"
+
+const currentChapterTitle = document.getElementById('document-title')
+const currentDocumentIFrame = document.getElementById('document-display')
+const chapterNamesList = document.getElementById('chapter-list')
+
+const chapterClickFunc = (chapterUrl) => {
+    return (e) => {
+        // Only if the chapter is not already displayed
+        if (currentChapterTitle.innerText !== e.target.innerText) {
+            // Set the current iframe's src to the chapter's url
+            currentDocumentIFrame.src = chapterUrl
+            // Set the title
+            currentChapterTitle.innerText = e.target.innerText
+        }
+    }
+}
+
+for (const chapter of chapters) {
+    // Create chapter list div
+    const chapEl = document.createElement('div')
+
+    // Use class name and insert title
+    chapEl.className = 'chapter-name'
+    chapEl.innerText = chapter.title
+
+    // Set onclick function and add to list
+    chapEl.onclick = chapterClickFunc(chapter.url)
+    chapterNamesList.appendChild(chapEl)
+}
+
+// Start on chapter 1
+currentChapterTitle.innerText = chapters[0].title
+currentDocumentIFrame.src = chapters[0].url
