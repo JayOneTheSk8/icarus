0cccdc1d7615cf14034f92e2076937df88b09497
JayOneTheSk8 <misterjustincox@gmail.com>
2022-05-13 13:46:16 -0400

    import chapters

---
+++ src/chapters.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        title: "Chapter 1: Elements of Surprise",
+        url: "https://docs.google.com/document/d/e/2PACX-1vSiHGgXpHJxO1qckESRXOUt_adlMs6I_2XnETQp5fnbswfMO9PMsMM4Tii5wyareH21fGAMEwMhoNjJ/pub?embedded=true"
+    },
+    {
+        title: "Chapter 2: Baby Bird",
+        url: "https://docs.google.com/document/d/e/2PACX-1vS258XjyRvs9e6tryYoksmcxCZLeeN1oFQvpHV6475jZE0ejNAQC7C2okfKSiDKY33wDnokANK2GVVE/pub?embedded=true"
+    },
+    {
+        title: "Chapter 3: Blood Brothers",
+        url: "https://docs.google.com/document/d/e/2PACX-1vRZQbR-PZduZtUK6EtRm9D2U7nBpRTohNl41npT035JCY3NXB8wXdJ2ZJWtq_g2imJV8PUnaVqekH2z/pub?embedded=true"
+    }
+]
