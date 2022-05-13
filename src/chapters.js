52ae59d166b2f3ed7f42aa80b235babe5ba36c21
JayOneTheSk8 <misterjustincox@gmail.com>
2022-05-13 19:18:29 -0400

    add chapters 4-10

--- src/chapters.js
+++ src/chapters.js
@@ -10,5 +10,33 @@ export default [
     {
         title: "Chapter 3: Blood Brothers",
         url: "https://docs.google.com/document/d/e/2PACX-1vRZQbR-PZduZtUK6EtRm9D2U7nBpRTohNl41npT035JCY3NXB8wXdJ2ZJWtq_g2imJV8PUnaVqekH2z/pub?embedded=true"
+    },
+    {
+        title: "Chapter 4: DISorientation",
+        url: "https://docs.google.com/document/d/e/2PACX-1vRUVkFjf33mbN2_3aXwABX9Pgye62haNynqJtX65GLqolifQxn0wB3wv8THHIPP0dYj_Trd38kuMhaH/pub?embedded=true"
+    },
+    {
+        title: "Chapter 5: The Man with Two Legs",
+        url: "https://docs.google.com/document/d/e/2PACX-1vRePNnutuum5x095jHcZrP9OsDqaShR3Yd0G7ArdhhWpiL5axcA2OFAMZ9WMQVQEWz26D6VrVvPBqS2/pub?embedded=true"
+    },
+    {
+        title: "Chapter 6: Fi(r)st Fight",
+        url: "https://docs.google.com/document/d/e/2PACX-1vTrvTvAeF_BdOmCuwkyn8D24WuvrBdPD7Tngcow0iDQIpKpKSXP1aCIlhV-ffF9gaCQL5fR2hEa_In6/pub?embedded=true"
+    },
+    {
+        title: "Chapter 7: Prep Time",
+        url: "https://docs.google.com/document/d/e/2PACX-1vRP5eadc5v1dGQy9uHkd3GOCwCFWCmvOeTnzqYsXkVJP6nFcIuZ3NtSx8Saikk2-fZe-MPMzmf7T-pM/pub?embedded=true"
+    },
+    {
+        title: "Chapter 8: Rush",
+        url: "https://docs.google.com/document/d/e/2PACX-1vS1Pt6FExLoGhaNpLyg6Pp8v-yE3olZuYaE7K3RhmK52poaZmi4JWdvQj-oo8ca9qyDfeQGLKeDLJF5/pub?embedded=true"
+    },
+    {
+        title: "Chapter 9: How the Days Breeze By",
+        url: "https://docs.google.com/document/d/e/2PACX-1vTiz9WdckEkAyaTW4ciEd578ZY-pRB5Qd7ZEQblWcf-tYR5_UZnjP1n_inmcgFtAlQgx4a8az9Rwrp-/pub?embedded=true"
+    },
+    {
+        title: "Chapter 10: Radiance",
+        url: "https://docs.google.com/document/d/e/2PACX-1vQDBUmWUdL431bNlzoXdFesg-xlB263nCv2FcuUqupVe4aQwADikSzJ9nWgojA6lgnEkeyik0-K2xKR/pub?embedded=true"
     }
 ]
