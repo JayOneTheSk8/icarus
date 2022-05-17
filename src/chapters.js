787f84a83d5eb5f1821d9ba4ab6f0bbbfbd882a5
JayOneTheSk8 <misterjustincox@gmail.com>
2022-05-17 02:41:52 -0400

    add chapter mp3Id's from drive

--- src/chapters.js
+++ src/chapters.js
@@ -1,42 +1,52 @@
 export default [
     {
         title: "Chapter 1: Elements of Surprise",
-        url: "https://docs.google.com/document/d/e/2PACX-1vSiHGgXpHJxO1qckESRXOUt_adlMs6I_2XnETQp5fnbswfMO9PMsMM4Tii5wyareH21fGAMEwMhoNjJ/pub?embedded=true"
+        url: "https://docs.google.com/document/d/e/2PACX-1vSiHGgXpHJxO1qckESRXOUt_adlMs6I_2XnETQp5fnbswfMO9PMsMM4Tii5wyareH21fGAMEwMhoNjJ/pub?embedded=true",
+        mp3Id: "1Kk9YMTGTtYagyPSBE3BvwWc_r8nulJrn"
     },
     {
         title: "Chapter 2: Baby Bird",
-        url: "https://docs.google.com/document/d/e/2PACX-1vS258XjyRvs9e6tryYoksmcxCZLeeN1oFQvpHV6475jZE0ejNAQC7C2okfKSiDKY33wDnokANK2GVVE/pub?embedded=true"
+        url: "https://docs.google.com/document/d/e/2PACX-1vS258XjyRvs9e6tryYoksmcxCZLeeN1oFQvpHV6475jZE0ejNAQC7C2okfKSiDKY33wDnokANK2GVVE/pub?embedded=true",
+        mp3Id: "1Ng9mmRaNG9g8X-A2WIJJ0whSr6OSCX-8"
     },
     {
         title: "Chapter 3: Blood Brothers",
-        url: "https://docs.google.com/document/d/e/2PACX-1vRZQbR-PZduZtUK6EtRm9D2U7nBpRTohNl41npT035JCY3NXB8wXdJ2ZJWtq_g2imJV8PUnaVqekH2z/pub?embedded=true"
+        url: "https://docs.google.com/document/d/e/2PACX-1vRZQbR-PZduZtUK6EtRm9D2U7nBpRTohNl41npT035JCY3NXB8wXdJ2ZJWtq_g2imJV8PUnaVqekH2z/pub?embedded=true",
+        mp3Id: "1SWQJhMYOeGaQv48DsfqfJOjNb1cnuVio"
     },
     {
         title: "Chapter 4: DISorientation",
-        url: "https://docs.google.com/document/d/e/2PACX-1vRUVkFjf33mbN2_3aXwABX9Pgye62haNynqJtX65GLqolifQxn0wB3wv8THHIPP0dYj_Trd38kuMhaH/pub?embedded=true"
+        url: "https://docs.google.com/document/d/e/2PACX-1vRUVkFjf33mbN2_3aXwABX9Pgye62haNynqJtX65GLqolifQxn0wB3wv8THHIPP0dYj_Trd38kuMhaH/pub?embedded=true",
+        mp3Id: "19IHspGqcyqs0_CEm_58Yxq749yoYb9Od"
     },
     {
         title: "Chapter 5: The Man with Two Legs",
-        url: "https://docs.google.com/document/d/e/2PACX-1vRePNnutuum5x095jHcZrP9OsDqaShR3Yd0G7ArdhhWpiL5axcA2OFAMZ9WMQVQEWz26D6VrVvPBqS2/pub?embedded=true"
+        url: "https://docs.google.com/document/d/e/2PACX-1vRePNnutuum5x095jHcZrP9OsDqaShR3Yd0G7ArdhhWpiL5axcA2OFAMZ9WMQVQEWz26D6VrVvPBqS2/pub?embedded=true",
+        mp3Id: "1B4hZ3VG-cwDEJQeemKnEpRpdDZs6rMVu"
     },
     {
         title: "Chapter 6: Fi(r)st Fight",
-        url: "https://docs.google.com/document/d/e/2PACX-1vTrvTvAeF_BdOmCuwkyn8D24WuvrBdPD7Tngcow0iDQIpKpKSXP1aCIlhV-ffF9gaCQL5fR2hEa_In6/pub?embedded=true"
+        url: "https://docs.google.com/document/d/e/2PACX-1vTrvTvAeF_BdOmCuwkyn8D24WuvrBdPD7Tngcow0iDQIpKpKSXP1aCIlhV-ffF9gaCQL5fR2hEa_In6/pub?embedded=true",
+        mp3Id: "1S6vfXt_pMbNP8PpWp1rAPtMupIIdYLV0"
     },
     {
         title: "Chapter 7: Prep Time",
-        url: "https://docs.google.com/document/d/e/2PACX-1vRP5eadc5v1dGQy9uHkd3GOCwCFWCmvOeTnzqYsXkVJP6nFcIuZ3NtSx8Saikk2-fZe-MPMzmf7T-pM/pub?embedded=true"
+        url: "https://docs.google.com/document/d/e/2PACX-1vRP5eadc5v1dGQy9uHkd3GOCwCFWCmvOeTnzqYsXkVJP6nFcIuZ3NtSx8Saikk2-fZe-MPMzmf7T-pM/pub?embedded=true",
+        mp3Id: "1G4EJ4_A9SVTm6v6GUKIEo7gkiu2yHLWm"
     },
     {
         title: "Chapter 8: Rush",
-        url: "https://docs.google.com/document/d/e/2PACX-1vS1Pt6FExLoGhaNpLyg6Pp8v-yE3olZuYaE7K3RhmK52poaZmi4JWdvQj-oo8ca9qyDfeQGLKeDLJF5/pub?embedded=true"
+        url: "https://docs.google.com/document/d/e/2PACX-1vS1Pt6FExLoGhaNpLyg6Pp8v-yE3olZuYaE7K3RhmK52poaZmi4JWdvQj-oo8ca9qyDfeQGLKeDLJF5/pub?embedded=true",
+        mp3Id: "1r2A2CJ4xiX4IzQfZdUgpqlrwt9q9lYTH"
     },
     {
         title: "Chapter 9: How the Days Breeze By",
-        url: "https://docs.google.com/document/d/e/2PACX-1vTiz9WdckEkAyaTW4ciEd578ZY-pRB5Qd7ZEQblWcf-tYR5_UZnjP1n_inmcgFtAlQgx4a8az9Rwrp-/pub?embedded=true"
+        url: "https://docs.google.com/document/d/e/2PACX-1vTiz9WdckEkAyaTW4ciEd578ZY-pRB5Qd7ZEQblWcf-tYR5_UZnjP1n_inmcgFtAlQgx4a8az9Rwrp-/pub?embedded=true",
+        mp3Id: "1cnZq9fzfgRU2TV9fH5v0VJQF9z0LFtRj"
     },
     {
         title: "Chapter 10: Radiance",
-        url: "https://docs.google.com/document/d/e/2PACX-1vQDBUmWUdL431bNlzoXdFesg-xlB263nCv2FcuUqupVe4aQwADikSzJ9nWgojA6lgnEkeyik0-K2xKR/pub?embedded=true"
+        url: "https://docs.google.com/document/d/e/2PACX-1vQDBUmWUdL431bNlzoXdFesg-xlB263nCv2FcuUqupVe4aQwADikSzJ9nWgojA6lgnEkeyik0-K2xKR/pub?embedded=true",
+        mp3Id: "1J-uWZvnqvMHAwRdFG1hzbRLAGgiASY8H"
     }
 ]
